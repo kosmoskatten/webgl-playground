4b24767c6a68c22d5d7b5c4a23eadbec6b0800e8
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-01-22 14:11:56 +0100

    More point sprite stuff.

--- simple-point-sprites/Main.elm
+++ simple-point-sprites/Main.elm
@@ -4,6 +4,8 @@ import Html exposing (Html, div, text)
 import Html.Attributes as Attr
 import Math.Vector3 exposing (Vec3, vec3)
 import WebGL exposing (..)
+import WebGL.Settings.DepthTest as DepthTest
+import WebGL.Settings.Blend as Blend
 
 
 type alias Model =
@@ -35,12 +37,18 @@ view model =
         [ WebGL.depth 1
         , WebGL.antialias
         , WebGL.alpha True
-        , WebGL.clearColor (42 / 255) (82 / 255) (190 / 255) 1
+        , WebGL.clearColor (51 / 255) (102 / 255) (204 / 255) 1
         ]
         [ Attr.width width, Attr.height height ]
-        [ WebGL.entity vertexShader
+        [ WebGL.entityWith [ DepthTest.default, Blend.add Blend.srcAlpha Blend.dstAlpha ]
+            vertexShader
             fragmentShader
-            (WebGL.points [ { position = vec3 0 0 0 } ])
+            (WebGL.points
+                [ { position = vec3 0.07 0 0.07 }
+                , { position = vec3 -0.07 0 -0.07 }
+                , { position = vec3 0 -0.07 0 }
+                ]
+            )
             {}
         ]
 
@@ -83,12 +91,13 @@ void main(void)
     vec2 centered = gl_PointCoord - vec2(0.5);
     float f = dot(centered, centered);
 
-    const vec4 color1 = vec4(0.6, 0.0, 0.0, 1.0);
-    const vec4 color2 = vec4(0.9, 0.7, 1.0, 1.0);
+    const vec4 color1 = vec4(0.2, 0.0, 0.8, 0.2);
+    const vec4 color2 = vec4(0.7, 0.7, 1.0, 0.3);
 
     if (f < 0.25)
     {
         gl_FragColor = mix(color1, color2, smoothstep(0.1, 0.25, f));
+        //gl_FragColor = mix(color1, color2, f);
         //gl_FragColor = color2;
     }
     else
