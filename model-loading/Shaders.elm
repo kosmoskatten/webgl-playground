85ddbc2673f1d0c03dabd4edc45942573653aa2b
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-02-01 14:40:22 +0100

    Added ambient light.

--- model-loading/Shaders.elm
+++ model-loading/Shaders.elm
@@ -1,11 +1,18 @@
 module Shaders exposing (vertexShader, fragmentShader)
 
 import Math.Vector3 exposing (Vec3)
+import Math.Vector4 exposing (Vec4)
 import Math.Matrix4 exposing (Mat4)
 import WebGL exposing (Shader)
 
 
-vertexShader : Shader { position : Vec3, normal : Vec3 } { mvp : Mat4 } {}
+vertexShader :
+    Shader
+        { position : Vec3
+        , normal : Vec3
+        }
+        { mvp : Mat4 }
+        { vAmbientColor : Vec4 }
 vertexShader =
     [glsl|
 attribute vec3 position;
@@ -13,20 +20,30 @@ attribute vec3 normal;
 
 uniform mat4 mvp;
 
+varying vec4 vAmbientColor;
+
+vec4 ambientColor()
+{
+  return vec4(255.0 / 255.0, 241.0 / 255.0, 224.0 / 255.0, 1.0) * 0.1;
+}
+
 void main(void)
 {
     gl_Position = mvp * vec4(position, 1.0);
+    vAmbientColor = ambientColor();
 }
 |]
 
 
-fragmentShader : Shader {} unif {}
+fragmentShader : Shader {} unif { vAmbientColor : Vec4 }
 fragmentShader =
     [glsl|
 precision mediump float;
 
+varying vec4 vAmbientColor;
+
 void main(void)
 {
-    gl_FragColor = vec4(0.5, 0.5, 0.5, 1.0);
+    gl_FragColor = vec4(0.1, 0.1, 0.1, 1.0) + vAmbientColor;
 }
 |]
