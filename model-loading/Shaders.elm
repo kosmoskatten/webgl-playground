f964d76ca27fbc2718b6dabca8acc0fe38b5be59
Patrik Sandahl <patrik.sandahl@ericsson.com>
2017-02-02 14:37:59 +0100

    Diffuse light in fragment shader.

--- model-loading/Shaders.elm
+++ model-loading/Shaders.elm
@@ -11,12 +11,12 @@ vertexShader :
         { position : Vec3
         , normal : Vec3
         }
-        { mvp : Mat4
-        , model : Mat4
-        , sunRayDirection : Vec3
+        { unif
+            | mvp : Mat4
+            , model : Mat4
         }
         { vAmbientColor : Vec4
-        , vDiffuseColor : Vec4
+        , vNormal : Vec3
         }
 vertexShader =
     [glsl|
@@ -25,42 +25,42 @@ attribute vec3 normal;
 
 uniform mat4 mvp;
 uniform mat4 model;
-uniform vec3 sunRayDirection;
 
 varying vec4 vAmbientColor;
-varying vec4 vDiffuseColor;
+varying vec3 vNormal;
 
 vec4 ambientColor()
 {
   return vec4(255.0 / 255.0, 241.0 / 255.0, 224.0 / 255.0, 1.0) * 0.1;
 }
 
-vec4 diffuseColor()
-{
-  vec3 rotatedNormal = normalize(vec3(model * vec4(normal, 0.0)));
-  float diff = max(dot(-sunRayDirection, rotatedNormal), 0.0);
-  return vec4(255.0 / 255.0, 241.0 / 255.0, 224.0 / 255.0, 1.0) * diff;
-}
-
 void main(void)
 {
     gl_Position = mvp * vec4(position, 1.0);
     vAmbientColor = ambientColor();
-    vDiffuseColor = diffuseColor();
+    vNormal = vec3(model * vec4(normal, 0.0));
 }
 |]
 
 
-fragmentShader : Shader {} unif { vAmbientColor : Vec4, vDiffuseColor : Vec4 }
+fragmentShader : Shader {} { unif | sunRayDirection : Vec3 } { vAmbientColor : Vec4, vNormal : Vec3 }
 fragmentShader =
     [glsl|
 precision mediump float;
 
+uniform vec3 sunRayDirection;
+
 varying vec4 vAmbientColor;
-varying vec4 vDiffuseColor;
+varying vec3 vNormal;
+
+vec4 diffuseColor()
+{
+  float diff = max(dot(-sunRayDirection, normalize(vNormal)), 0.0);
+  return vec4(255.0 / 255.0, 241.0 / 255.0, 224.0 / 255.0, 1.0) * diff * 0.5;
+}
 
 void main(void)
 {
-    gl_FragColor = vec4(0.1, 0.1, 0.1, 1.0) + vAmbientColor + vDiffuseColor;
+    gl_FragColor = vec4(0.1, 0.1, 0.1, 1.0) + vAmbientColor + diffuseColor();
 }
 |]
